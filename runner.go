cd7925b1e51a9be4c85d47fc1d79a942d19debfc
Stefan Warman <stefan.warman@fraugster.com>
2017-09-20 13:19:20 +0200

    Revert change to run/sleep ordering to avoid tight loop when errors occur

--- runner.go
+++ runner.go
@@ -104,31 +104,29 @@ func (r *Runner) Run() {
 			}
 		}
 
-		//if this is the first time it has run then skip the interval and just run straight away.
-		if len(r.stats.Views) > 0 {
-			// by default the aggregates are checked at the floor interval (as defined by the constructor) to see if they
-			// need to be run.
-			runInterval := r.runIntervalFloor
-			if r.intervalBackoffEnabled {
-				// however if back-off is enabled the longer the last run took, the longer the interval becomes. The
-				// interval scales in line with the last run duration so if a run takes 5 minutes and the floor is 1
-				// minute the next interval will be 5 minutes. This will scale back number of runs when all tasks appear
-				// to be slowing down and overloading the DB.
-				if lastInterval := time.Duration(r.stats.LastRunSeconds) * time.Second; lastInterval > r.runIntervalFloor {
-					//limit the max run interval by the ceiling
-					if lastInterval.Seconds() > r.runIntervalCeiling.Seconds() {
-						runInterval = r.runIntervalCeiling
-					} else {
-						runInterval = lastInterval
-					}
-				}
-			}
-			time.Sleep(runInterval)
-		}
-
 		if err := r.runOnce(false); err != nil {
 			log.Logf("runner encountered error: %s", err.Error())
 		}
+
+		// by default the aggregates are checked at the floor interval (as defined by the constructor) to see if they
+		// need to be run.
+		runInterval := r.runIntervalFloor
+		if r.intervalBackoffEnabled {
+			// however if back-off is enabled the longer the last run took, the longer the interval becomes. The
+			// interval scales in line with the last run duration so if a run takes 5 minutes and the floor is 1
+			// minute the next interval will be 5 minutes. This will scale back number of runs when all tasks appear
+			// to be slowing down and overloading the DB.
+			if lastInterval := time.Duration(r.stats.LastRunSeconds) * time.Second; lastInterval > r.runIntervalFloor {
+				//limit the max run interval by the ceiling
+				if lastInterval.Seconds() > r.runIntervalCeiling.Seconds() {
+					runInterval = r.runIntervalCeiling
+				} else {
+					runInterval = lastInterval
+				}
+			}
+		}
+		time.Sleep(runInterval)
+
 	}
 }
 
